3b5cb74ceb58437b58032a2f81c96e7f4b6980f9
marcosgama <marcosdanielgama@gmail.com>
2024-10-11 18:19:02 -0300

    limit gun shot speed

--- player.py
+++ player.py
@@ -1,7 +1,14 @@
 import pygame
 
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import (
+    PLAYER_RADIUS,
+    PLAYER_TURN_SPEED,
+    PLAYER_SPEED,
+    PLAYER_SHOOT_SPEED,
+    PLAYER_SHOOT_COOLDOWN,
+)
+
 from shot import Shot
 
 
@@ -9,6 +16,7 @@ class Player(CircleShape):
     def __init__(self, x: int, y: int) -> None:
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self._gun_timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -26,6 +34,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self._gun_timer -= dt
 
         if keys[pygame.K_a]:
             self.rotate(dt * -1)
@@ -40,7 +49,9 @@ class Player(CircleShape):
             self.move(dt * -1)
 
         if keys[pygame.K_k]:
-            self.shoot()
+            if self._gun_timer <= 0:
+                self.shoot()
+                self._gun_timer = PLAYER_SHOOT_COOLDOWN
 
     def move(self, dt: int):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
